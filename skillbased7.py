409c383c63923ee2f7c78da62b0d5d69a37977cc
Aryan447 <89701129+Aryan447@users.noreply.github.com>
2022-11-24 21:39:29 +0530

    Update skillbased7.py

--- skillbased7.py
+++ skillbased7.py
@@ -1,17 +1,12 @@
-import random
-import math
+import random, math
 
-# Taking Inputs of Lower Bound
-lower = int(input("Enter Lower bound:- "))
+lowerBound = int(input("Enter Lower bound: "))
+upperBound = int(input("Enter Upper bound: "))
 
-# Taking Inputs of Upper Bound
-upper = int(input("Enter Upper bound:- "))
-
-# generating random number between
-# lower and upper bound
-x = random.randint(lower, upper)
+# generating random number between lower and upper bound
+var = random.randint(lowerBound, upperBound)
 print("\n\tYou've only ",
-	round(math.log(upper - lower + 1, 2)),
+	round(math.log(upperBound - lowerBound + 1, 2)),
 	" chances to guess the integer!\n")
 
 # Initializing the number of guesses
@@ -19,29 +14,29 @@ count = 0
 
 # for calculation of minimum number of
 # guesses depends upon range
-while count < math.log(upper - lower + 1, 2):
+while count < math.log(upperBound - lowerBound + 1, 2):
 	count += 1
 
 	# taking guessing number as input
-	guess = int(input("Guess a number:- "))
+	guess = int(input("Guess a number: "))
 
 	# Condition testing
-	if x == guess:
-		print("Congratulations you did it in ",
+	if var == guess:
+		print("YOU WON, you guessed in ",
 			count, " try")
-		# Once guessed, loop will break
 		break
 
-	elif x > guess:
-		print("You guessed too small!")
-	elif x < guess:
-		print("You Guessed too high!")
+
+	elif var > guess:
+		print("Your guess is lower")
+
+	elif var < guess:
+		print("Your guess is higher")
 
 # If Guessing is more than required guesses,
 # shows this output.
-if count >= math.log(upper - lower + 1, 2):
-	print("\nThe number is %d" % x)
-	print("\tBetter Luck Next time!")
+if count >= math.log(upperBound - lowerBound + 1, 2):
+	print("\nThe number is %d" % var)
 
 print("\n")
 print("Name = Aryan Singh")
