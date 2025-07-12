c7ed91d9e060fc58a801eda91cef42bbdbebd368
Aryan447 <singharyan4477@gmail.com>
2025-07-12 18:04:47 +0530

    automated dev commit

--- leetcode.py
+++ leetcode.py
@@ -1,39 +1,24 @@
 '''
-Input: head = [1,2,3,4,5], k = 2
-Output: [4,5,1,2,3]
+Example 1:
+Input: g = [1,2,3], s = [1,1]
+Output: 1
+Explanation: You have 3 children and 2 cookies. The greed factors of 3 children are 1, 2, 3.
+And even though you have 2 cookies, since their size is both 1, you could only make the child whose greed factor is 1 content.
+You need to output 1.
+
+Example 2:
+Input: g = [1,2], s = [1,2,3]
+Output: 2
+Explanation: You have 2 children and 3 cookies. The greed factors of 2 children are 1, 2.
+You have 3 cookies and their sizes are big enough to gratify all of the children,
+You need to output 2.
 '''
 from typing import List, Optional
 
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
 class Solution:
-    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        if not head:
-            return head
-        length = 1
-        dummy = head
-        while dummy.next:
-            dummy = head.next
-            length += 1
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort(reverse=True)
 
-        position =
-
-
-node1 = ListNode(1)
-node2 = ListNode(2)
-node3 = ListNode(3)
-node4 = ListNode(4)
-node5 = ListNode(5)
-
-node1.next = node2
-node2.next = node3
-node3.next = node4
-node4.next = node5
-
-mylist = node1
 
 mysol = Solution()
-print(mysol.rotateRight(node1, 2))
+print(mysol.findContentChildren([1, 2, 3], [1, 1]))
