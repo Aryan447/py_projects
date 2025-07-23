c629a61aacbde2af073c55145570d85006939bef
Aryan447 <singharyan4477@gmail.com>
2025-07-23 19:01:01 +0530

    automated dev commit

--- leetcode.py
+++ leetcode.py
@@ -1,54 +1,34 @@
 '''
-Input: bills = [5,5,5,10,20]
-Output: true
-Explanation:
-From the first 3 customers, we collect three $5 bills in order.
-From the fourth customer, we collect a $10 bill and give back a $5.
-From the fifth customer, we give a $10 bill and a $5 bill.
-Since all customers got correct change, we output true.
-From the first two customers in order, we collect two $5 bills.
-Input: bills = [5,5,10,10,20]
 
-Output: false
-Explanation:
-For the next two customers in order, we collect a $10 bill and give back a $5 bill.
-For the last customer, we can not give the change of $15 back because we only have two $10 bills.
-Since not every customer received the correct change, the answer is false.
 '''
 from typing import List, Optional
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
-    def lemonadeChange(self, bills: List[int]) -> bool:
-        if bills[0] != 5:
-            return False
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
-        fiveDollars = 0
-        tenDollars = 0
 
-        for x in bills:
-            if x == 5:
-                fiveDollars += 1
 
-            elif x == 10:
-                if fiveDollars > 0:
-                    fiveDollars -= 1
-
-                else:
-                    return False
-
-                tenDollars += 1
-            else:
-                if fiveDollars > 0 and tenDollars > 0:
-                    fiveDollars -= 1
-                    tenDollars -= 1
-
-                elif fiveDollars > 2:
-                    fiveDollars -= 3
-                else:
-                    return False
-            print(fiveDollars, tenDollars)
-
-        return True
+obj15 = ListNode(16, obj16)
+obj14 = ListNode(15, obj15)
+obj13 = ListNode(14, obj14)
+obj12 = ListNode(13, obj13)
+obj11 = ListNode(12, obj12)
+obj10 = ListNode(11, obj11)
+obj9 = ListNode(10, obj10)
+obj8 = ListNode(9, obj9)
+obj7 = ListNode(8, obj8)
+obj6 = ListNode(7, obj7)
+obj5 = ListNode(6, obj6)
+obj4 = ListNode(5, obj5)
+obj3 = ListNode(4, obj4)
+obj2 = ListNode(3, obj3)
+obj1 = ListNode(2, obj2)
+head = ListNode(1, obj1)
 
 mysol = Solution()
-print(mysol.lemonadeChange([5, 5, 5, 10, 20]))
+print(mysol.sortList(head))
---
+++ practice.py
@@ -0,0 +1,21 @@
+'''
+Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+For example, the square matrix
+
+is shown below:
+
+1 2 3
+4 5 6
+9 8 9
+
+The left-to-right diagonal = 15
+The right-to-left diagonal = 17
+Their absolute difference is 2
+'''
+
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    # Write your code here
+
+
+array = [73, 67, 38, 33]
+print(countApplesAndOranges(array))
